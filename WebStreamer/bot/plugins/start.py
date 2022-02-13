d9aa48d54fd523aadff4131e9fe910bef241f310
xalienxx <95128665+xalienxx@users.noreply.github.com>
2022-02-13 18:05:31 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -178,7 +178,7 @@ async def help_handler(bot, message):
                 disable_web_page_preview=True)
             return
     await message.reply_text(
-        text="Send/Forward to me any File I will provide External Direct Download Link!\n\nCheck bot status : https://cutt.ly/JIY88jo [Password: Mikasa]",
+        text="Send/Forward to me any File I will provide External Direct Download Link!\n\nCheck bot status : https://cutt.ly/vPrSzFf",
         parse_mode="Markdown",
         disable_web_page_preview=True,
         #reply_markup=InlineKeyboardMarkup(
