219783fda9a7d3e83dda446702e81cde925c5b82
xalienxx <95128665+xalienxx@users.noreply.github.com>
2022-05-31 19:43:04 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -71,10 +71,10 @@ async def start(b, m):
                     disable_web_page_preview=True)
                 return
         await m.reply_text(
-            text='Hi!!\nI am Instant Telegram File to Link Generator Bot.Please check /help options.\n\nSend/forward to me any file & \nsee the magic!🔮\n\nFor Start streaming server use /startbot command \n\nMaintenance by @XHunterK 🦅 @TeamNezuko 🇱🇰',
+            text='Mikasa Server 3 Connected./nPlease check /help options.\n\nMaintenance by @XHunterK 🦅 @TeamNezuko 🇱🇰/n/n22-5-31 Pinging Started...',
              reply_markup=InlineKeyboardMarkup(
                  [
-                     [InlineKeyboardButton('📌 Devs Channel', url='https://t.me/TeamNezuko')], #InlineKeyboardButton('Support Group', url='https://t.me/*****')],
+                     #[InlineKeyboardButton('📌 Devs Channel', url='https://t.me/TeamNezuko')], #InlineKeyboardButton('Support Group', url='https://t.me/*****')],
                      [InlineKeyboardButton('📌 Mikasa 2.0 Project', url='https://t.me/TeamNezuko/62')]
                  ]
              ),
